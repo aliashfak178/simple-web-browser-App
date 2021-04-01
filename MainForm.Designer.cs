388f34e8502afb8153e71630669ce25920fc6442
aliashfak178 <aliashfak178@gmail.com>
2021-04-01 20:45:42 +0530

    init

---
+++ MainForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Web_Browser_Demo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.HomeToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.SearchToolStripComboBox = new System.Windows.Forms.ToolStripComboBox();
+            this.GoToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.WebBrowsers = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(863, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 24);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(863, 461);
+            this.webBrowser.TabIndex = 1;
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.WebBrowsers);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.webBrowser1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(863, 436);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 24);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(863, 461);
+            this.toolStripContainer1.TabIndex = 2;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.HomeToolStripButton,
+            this.SearchToolStripComboBox,
+            this.GoToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(491, 25);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // HomeToolStripButton
+            // 
+            this.HomeToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.HomeToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeToolStripButton.Image")));
+            this.HomeToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.HomeToolStripButton.Name = "HomeToolStripButton";
+            this.HomeToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.HomeToolStripButton.Click += new System.EventHandler(this.HomeToolStripButton_Click);
+            // 
+            // SearchToolStripComboBox
+            // 
+            this.SearchToolStripComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.SearchToolStripComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
+            this.SearchToolStripComboBox.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.SearchToolStripComboBox.Name = "SearchToolStripComboBox";
+            this.SearchToolStripComboBox.Size = new System.Drawing.Size(400, 25);
+            this.SearchToolStripComboBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SearchToolStripComboBox_KeyUp);
+            // 
+            // GoToolStripButton
+            // 
+            this.GoToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.GoToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("GoToolStripButton.Image")));
+            this.GoToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.GoToolStripButton.Name = "GoToolStripButton";
+            this.GoToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.GoToolStripButton.Click += new System.EventHandler(this.GoToolStripButton_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(863, 436);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // WebBrowsers
+            // 
+            this.WebBrowsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WebBrowsers.Location = new System.Drawing.Point(0, 0);
+            this.WebBrowsers.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WebBrowsers.Name = "WebBrowsers";
+            this.WebBrowsers.ScriptErrorsSuppressed = true;
+            this.WebBrowsers.Size = new System.Drawing.Size(863, 436);
+            this.WebBrowsers.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 485);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Ali Web Browser";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton HomeToolStripButton;
+        private System.Windows.Forms.ToolStripComboBox SearchToolStripComboBox;
+        private System.Windows.Forms.ToolStripButton GoToolStripButton;
+        private System.Windows.Forms.WebBrowser WebBrowsers;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Web_Browser_Demo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("All right Reserved with Ali Tech 2020-2021",
+                "About  Ali Web Brwser ",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            WebBrowsers.Navigate("https://www.google.co.in");
+        }
+
+        private void HomeToolStripButton_Click(object sender, EventArgs e)
+        {
+            WebBrowsers.Navigate("https://www.google.co.in");
+        }
+
+        private void GoToolStripButton_Click(object sender, EventArgs e)
+        {
+            WebBrowsers.Navigate(SearchToolStripComboBox.Text);
+        }
+
+        private void SearchToolStripComboBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                WebBrowsers.Navigate(SearchToolStripComboBox.Text);
+            }
+        }
+    }
+}
